8d92e1d8fafce3cbdbe479aee1710a01ea15b819
Alex Berliner <alexberliner@gmail.com>
2026-02-23 13:43:08 -0500

    first time setup

---
+++ fts.sh
@@ -0,0 +1,7 @@
+function fts {
+    sudo apt update -y
+    sudo apt upgrade -y
+    sudo apt install -y gcc-arm-none-eabi
+    sudo apt install -y gcc g++ cmake make bison git git-cola gitk vim
+    sudo apt install -y wireguard
+}
